776b4fb1541a8336fd9fbde18a382fd6492b2b19
Xuheng Duan <spicyowlmichael@gmail.com>
2025-04-26 23:01:55 -0400

    simple text transmiter

--- challenge/server.py
+++ challenge/server.py
@@ -3,53 +3,59 @@ from aiohttp import web
 from aiortc import RTCPeerConnection, RTCSessionDescription
 from ballAnimation import BallVideoStreamTrack
 
+class VideoServer:
+    def __init__(self):
+        self.pcs = set()
+        self.ball_track = BallVideoStreamTrack()
+        # Start ball animation
+        asyncio.create_task(self.animate_ball())
 
-async def handle_offer(request):
-    offer_data = await request.json()
-    offer = RTCSessionDescription(sdp=offer_data['sdp'], type=offer_data['type'])
-    pc = RTCPeerConnection()
-    
-    # Create the ball animation track
-    ball_track = BallVideoStreamTrack(fps=30)
-    
-    @pc.on('datachannel')
-    def on_datachannel(channel):
-        print(f"Channel {channel.label} established")
-        cnt = [0]
+    async def animate_ball(self):
+        while True:
+            await self.ball_track.recv()  # This updates the ball position
+            await asyncio.sleep(1/30)  # 30 FPS
+
+    async def handle_offer(self, request):
+        offer_data = await request.json()
+        offer = RTCSessionDescription(sdp=offer_data['sdp'], type=offer_data['type'])
+        pc = RTCPeerConnection()
+        self.pcs.add(pc)
         
-        async def send_periodic_messages():
-            while True:
-                try:
-                    # Get the current ball position from our track
-                    ball_pos = ball_track.get_ball_position()  # Returns [x, y]
-                    message = f"Hello from server! Count: {cnt[0]}, ball position: ({ball_pos[0]}, {ball_pos[1]})"
-                    print(f"Server sending: {message}")
-                    channel.send(message)
-                    cnt[0] += 1
-                    await asyncio.sleep(2)
-                except Exception as e:
-                    print(f"Error sending message: {e}")
-                    break
+        @pc.on('datachannel')
+        def on_datachannel(channel):
+            cnt = [0]
+            
+            async def send_periodic_messages():
+                while True:
+                    try:
+                        ball_pos = self.ball_track.get_ball_position()
+                        message = f"Hello from server! Count: {cnt[0]}, ball position: ({ball_pos[0]}, {ball_pos[1]})"
+                        channel.send(message)
+                        print(message)
+                        cnt[0] += 1
+                        await asyncio.sleep(2)
+                    except Exception as e:
+                        print(f"Error sending message: {e}")
+                        break
 
-        @channel.on('message')
-        def on_message(message):
-            print(f"Client says: {message}")
-            asyncio.create_task(send_periodic_messages())
+            @channel.on('message')
+            def on_message(message):
+                print(f"Client says: {message}")
+                if cnt[0] == 0:
+                    asyncio.create_task(send_periodic_messages())
 
-        # Keep the ball animation running
-        asyncio.create_task(ball_track.recv())
-
-    await pc.setRemoteDescription(offer)
-    answer = await pc.createAnswer()
-    await pc.setLocalDescription(answer)
-    return web.json_response({
-        "sdp": pc.localDescription.sdp,
-        "type": pc.localDescription.type
-    })
+        await pc.setRemoteDescription(offer)
+        answer = await pc.createAnswer()
+        await pc.setLocalDescription(answer)
+        return web.json_response({
+            "sdp": pc.localDescription.sdp,
+            "type": pc.localDescription.type
+        })
 
 async def main():
+    server = VideoServer()
     app = web.Application()
-    app.router.add_post("/offer", handle_offer)
+    app.router.add_post("/offer", server.handle_offer)
     runner = web.AppRunner(app)
     await runner.setup()
     await web.TCPSite(runner, "localhost", 8080).start()
